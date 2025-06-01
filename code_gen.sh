7ba4919eea31b233791ef8e678301112ddc83d0b
budgy11 <birdbudge@gmail.com>
2025-06-01 15:00:57 -0500

    generated code snippets

--- code_gen.sh
+++ code_gen.sh
@@ -2,11 +2,12 @@
 
 languages=("php" "ruby" "csharp" "java" "javascript")
 models=("llama3.1" "deepseek-r1" "gemma3")
-
+ctr=1
 while read -r prompt; do 
         for i in "${languages[@]}"; do
                 for j in "${models[@]}"; do
-                        ./main.py -p "$prompt using $i" -m $j >> $i/$j.jsonl
+                        ./main.py -p "$prompt using $i" -m $j >> $i/$j-$ctr.json
                 done
         done
+        ((ctr++))
 done < ./test_prompts.txt
