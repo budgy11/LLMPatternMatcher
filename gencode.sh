f9386fa5fce9615393ec548a2a191cd4e88a535b
budgy11 <birdbudge@gmail.com>
2025-06-08 23:00:00 -0500

    missing last two prompts and 404-500 deepseek on purchasing

--- gencode.sh
+++ gencode.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+#TODO MISSING 404-500 DEEPSEEK PURCHASING
+
 models=("llama3.1" "gemma3" "deepseek-r1:32b")
+
 while read -r line;
 do
         for model in ${models[@]};
         do
-                for i in {1..100};
+                for i in {101..500};
                 do
                         ./main.py -p "$(echo -n $line | awk -F',' '{print $1}')" -m $model > "$(echo -n $line | awk -F',' '{print $2}')/$model-$i.json"
                 done
         done
-done < 100_prompts.txt
+done < last_two.txt
