ce6c598c88b0d82e06848f0ea23bcf6cb2f5b572
budgy11 <birdbudge@gmail.com>
2025-06-14 14:51:33 -0500

    Moved variable classification to rules.py

--- rules.py
+++ rules.py
@@ -13,3 +13,13 @@ regex_rules = {
         "There is a password variable in use"
         ]
 }
+
+#VARIABLE CLASSIFICATIONS
+#for variables that usually store sensitive data in code1
+sensitive = []
+
+#for variables that usually take user input prior to or without user
+dangerous = []
+
+#for variables that always use safe input or hardcoded values
+safe = []
