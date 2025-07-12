a56659282a83dac285f6f1381c67f81075f6ced0
budgy11 <birdbudge@gmail.com>
2025-07-12 15:21:56 -0500

    more comment cleanup

--- code_parser.py
+++ code_parser.py
@@ -9,14 +9,11 @@ def pull_code(output):
     return generated
 
 def code_parse(block, quiet):
-   #print(regex_rules['use_echo'])
    alert_string = ""
    for rule in regex_rules:
-      #precompiled patterns could help with runtime if necessary
       pattern = re.compile(rf"({regex_rules[rule][0]})", re.IGNORECASE)
       if not quiet and pattern.search(block):
          alert_string += regex_rules[rule][1]+"\n"
-      #print("rule found: %s" % regex_rules[rule][0])
       block = pattern.sub(r"\033[0;31m\1\033[0m",block)
    return block,alert_string
 
